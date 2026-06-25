2f765c5be58a8d329f5d951951e5115cff9cf551
Sumit Thakur <tsumit0505@gmail.com>
2026-06-25 13:27:47 +0530

    Architecture change

--- backend/services/aiService.js
+++
@@ -1,7 +0,0 @@
-import { GoogleGenAI } from "@google/genai";
-import dotenv from "dotenv";
-dotenv.config();
-const client = new GoogleGenAI({
-    apiKey: process.env.GEMINI_API_KEY,
-});
-export default client;
